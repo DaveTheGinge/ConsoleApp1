b06606e47f34ada4ff78367515fb317835bdeb0e
DaveTheGinge <47980192+DaveTheGinge@users.noreply.github.com>
2021-07-15 20:20:52 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Interact()
+        {
+            Console.WriteLine("Is there an echo in here?");
+            String response = Console.ReadLine();
+            Console.WriteLine(response);
+            WaitForEnter();
+        }
+
+        private static void WaitForEnter()
+        {
+            Console.WriteLine("Press enter to exit... ");
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Interact();
+        }
+
+    }
+}
